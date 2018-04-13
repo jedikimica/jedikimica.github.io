6219b4205ba8d809f7c0b253d28c099257d0e930
Kim Tubola <ktubola@gmail.com>
2018-04-13 12:07:49 -0700

    update

--- project1/assets/food.js
+++ project1/assets/food.js
@@ -4,19 +4,20 @@
  */
 function buildQueryURL() {
     // queryURL is the url we'll use to query the API
-    var queryURL = "https://pixabay.com/api/";
+    var queryURL = "http://food2fork.com/api/search";
   
     // add the api key parameter (the one we received when we registered)
-    queryURL += "?key=8677244-dfd7cfd38bf475092dfe14341";
+    queryURL += "?key=0e2e87e62bf49881c143d53640d595e9";
   
     // grab text the user typed into the search input, add as parameter to url
     var searchTerm = $("#breakfast").val().trim();
-    queryURL += "&q=" + searchTerm + "&image_type=photo";
+    queryURL += "&q=" + searchTerm;
 }  
 var queryURL = buildQueryURL();
 
 
 $.ajax({
+    
     url: queryURL,
     method: "GET"
   })
@@ -25,15 +26,15 @@ $.ajax({
      .then(function(response) {
 
         // Saving the image_original_url property
-          var imageUrl = response.data.imageUrl;
+          var imageUrl = response.recipes.image_url;
 
           // Creating and storing an image tag
           var breakfastImage = $("<img>");
 
           // Setting the catImage src attribute to imageUrl
           breakfastImage.attr("src", imageUrl);
-          breakfastImage.attr("alt", "cat image");
+          breakfastImage.attr("alt", "breakfast image");
 
           // Prepending the catImage to the images div
-          $("#images").prepend(catImage);
+          $("#images").prepend(breakfastImage);
         });
