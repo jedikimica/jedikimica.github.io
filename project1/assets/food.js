a3ec37dbceab78a4e2591cd02f549b40318d1aea
Kim Tubola <ktubola@gmail.com>
2018-04-13 20:02:51 -0700

    10

--- project1/assets/food.js
+++ project1/assets/food.js
@@ -40,7 +40,7 @@ $.ajax({
        console.log(response)
 
         // Saving the image_original_url property
-          var imageUrl = response.recipes[1].image_url;
+          var imageUrl = response.recipes[10].image_url;
 
           var test = response.recipes
 
